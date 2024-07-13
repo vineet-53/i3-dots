cfaa11d45c4fab53d9f524687d5162c6ba055d18
vineet-53 <vineetsinghnegi53@gmail.com>
2024-07-13 09:00:18 +0530

    configure nvim tree

--- nvim/.config/nvim/lua/coder/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/coder/plugins/nvim-tree.lua
@@ -1,122 +1,65 @@
-local function my_attach()
-  local status_ok, nvim_tree = pcall(require, "nvim-tree")
-  if not status_ok then
-    return
-  end
-
-  local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
-  if not config_status_ok then
-    return
-  end
-
-  local tree_cb = nvim_tree_config.nvim_tree_callback
-
-  nvim_tree.setup({
-    disable_netrw = true,
-    hijack_netrw = true,
-    open_on_setup = false,
-    ignore_ft_on_setup = {
-      "startify",
-      "dashboard",
-      "alpha",
-    },
-    auto_close = true,
-    open_on_tab = false,
-    hijack_cursor = false,
-    update_cwd = true,
-    update_to_buf_dir = {
-      enable = true,
-      auto_open = true,
-    },
-    diagnostics = {
-      enable = true,
-      icons = {
-        hint = "",
-        info = "",
-        warning = "",
-        error = "",
-      },
-    },
-    update_focused_file = {
-      enable = true,
-      update_cwd = true,
-      ignore_list = {},
-    },
-    system_open = {
-      cmd = nil,
-      args = {},
-    },
-    filters = {
-      dotfiles = false,
-      custom = {},
-    },
-    git = {
-      enable = true,
-      ignore = true,
-      timeout = 500,
-    },
-    view = {
-      width = 30,
-      height = 30,
-      hide_root_folder = false,
-      side = "left",
-      auto_resize = true,
-      mappings = {
-        custom_only = false,
-        list = {
-          { key = { "o", "<CR>", "t" }, cb = tree_cb("edit") },
-        },
-      },
-      number = false,
-      relativenumber = false,
-    },
-    trash = {
-      cmd = "trash",
-      require_confirm = true,
-    },
-    quit_on_open = 0,
-    git_hl = 1,
-    disable_window_picker = 0,
-    root_folder_modifier = ":t",
-    show_icons = {
-      git = 1,
-      folders = 1,
-      files = 1,
-      folder_arrows = 1,
-      tree_width = 30,
-    },
-  })
-end
 return {
   "nvim-tree/nvim-tree.lua",
-  enabled = false,
-  dependencies = {
-    "nvim-tree/nvim-tree.lua",
-    "nvim-tree/nvim-web-devicons",
-  },
+  dependencies = "nvim-tree/nvim-web-devicons",
   config = function()
-    require("nvim-tree").setup({
-      on_attach = my_attach(),
-    })
-    vim.g.nvim_tree_icons = {
-      default = "",
-      symlink = "",
+    local nvimtree = require("nvim-tree")
+    -- recommended settings from nvim-tree documentation
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+    nvimtree.setup({
+      on_attach = function(bufnr)
+        local api = require("nvim-tree.api")
+
+        local function opts(desc)
+          return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+        end
+        -- default mappings
+        api.config.mappings.default_on_attach(bufnr)
+        -- custom keymaps
+        vim.keymap.set("n", "?", api.tree.toggle_help, opts("Help"))
+      end,
+      view = {
+        width = 30,
+        relativenumber = false,
+      },
+      -- change folder arrow icons
+      renderer = {
+        indent_markers = {
+          enable = true,
+        },
+        icons = {
+          glyphs = {
+            folder = {
+              arrow_closed = "", -- arrow when folder is closed
+              arrow_open = "", -- arrow when folder is open
+            },
+          },
+        },
+      },
+      -- disable window_picker for
+      -- explorer to work well with
+      -- window splits
+      actions = {
+        open_file = {
+          window_picker = {
+            enable = false,
+          },
+        },
+      },
+      filters = {
+        dotfiles = true,
+        custom = { ".DS_Store" },
+      },
       git = {
-        unstaged = "",
-        staged = "S",
-        unmerged = "",
-        renamed = "➜",
-        deleted = "",
-        untracked = "U",
-        ignored = "◌",
+        ignore = false,
       },
-      folder = {
-        default = "",
-        open = "",
-        empty = "",
-        empty_open = "",
-        symlink = "",
-      },
-    }
+    })
+
+    -- set keymaps
+    local keymap = vim.keymap -- for conciseness
+
+    keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
+    keymap.set("n", "<leader>f", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
+    keymap.set("n", "<leader>r", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
   end,
 }
