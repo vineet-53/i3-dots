9b6c61da12573f795012698f9aa87a1963f431d9
vineet-53 <vineetsinghnegi53@gmail.com>
2024-07-13 12:43:49 +0530

    changes

--- nvim/.config/nvim/lua/coder/plugins/oil.lua
+++ nvim/.config/nvim/lua/coder/plugins/oil.lua
@@ -5,7 +5,7 @@ return {
     vim.keymap.set("n", "<leader>e", oil.toggle_float, { desc = "open explorer" })
     vim.keymap.set("n", "t", oil.select, { desc = "select the file" })
     -- vim.keymap.set("n", "<C-p>", oil.preview, { desc = "preview the file" })
-    vim.keymap.set("n", "<C-w><C-h>", oil.toggle_hidden, { desc = "toggle hidden files" })
+    vim.keymap.set("n", "<leader>h", oil.toggle_hidden, { desc = "toggle hidden files" })
     oil.setup()
   end,
 }
